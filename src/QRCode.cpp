b27d2a84b63c112c4785325335e47c192496629b
Lucas Pujol <lucas.pujol@epitech.eu>
2026-02-07 20:00:49 +0100

    feat(QRCode): enhance QRCode class with encoding mode identification and validation methods

--- src/QRCode.cpp
+++ src/QRCode.cpp
@@ -1,5 +1,6 @@
 #include "QRCode.hpp"
 #include <iostream>
+#include <algorithm>
 
 QRCode::QRCode(const std::string &src) {
     _src = src;
@@ -9,8 +10,39 @@ QRCode::~QRCode() {}
 
 void QRCode::generate() {
     std::cout << "generating qrcode for: " << _src << std::endl;
+    _mode = identifyEncodingMode();
+    std::cout << "encoding mode: " << EncodingModeToString(_mode) << std::endl;
 }
 
 void QRCode::save(const std::string &filepath) {
     std::cout << "saving qrcode into: " << filepath << std::endl;
+}
+
+EncodingMode QRCode::identifyEncodingMode() const {
+    if (isNumeric(_src))        return EncodingMode::Numeric;
+    if (isAlphanumeric(_src))   return EncodingMode::Alphanumeric;
+    return EncodingMode::Binary;
+}
+
+bool QRCode::isAlphanumeric(const std::string &string) const {
+    if (string.empty()) return false;
+    for (char c : string) {
+        if (alphanumericChars.find(c) == std::string::npos)
+            return false;
+    }
+    return true;
+}
+
+bool QRCode::isNumeric(const std::string &string) const {
+    return !string.empty() &&
+        std::all_of(string.begin(), string.end(), ::isdigit);
+}
+
+std::string QRCode::EncodingModeToString(EncodingMode mode) const {
+    switch (mode) {
+        case EncodingMode::Numeric: return "Numeric";
+        case EncodingMode::Alphanumeric: return "Alphanumeric";
+        case EncodingMode::Binary: return "Binary";
+        default: return "Unknown";
+    }
 }
--- src/QRCode.hpp
+++ src/QRCode.hpp
@@ -2,6 +2,19 @@
 
 #include <string>
 
+enum class EncodingMode {
+    Numeric,
+    Alphanumeric,
+    Binary,
+};
+
+enum class CorrectionLevel {
+    L = 7,
+    M = 15,
+    Q = 25,
+    H = 30,
+};
+
 class QRCode {
     public:
         QRCode(const std::string &src);
@@ -9,7 +22,17 @@ class QRCode {
 
         void generate();
         void save(const std::string &filepath);
+        EncodingMode identifyEncodingMode() const;
 
     private:
         std::string _src;
+        EncodingMode _mode;
+        CorrectionLevel _correctionLevel;
+        int _version;
+
+        std::string EncodingModeToString(EncodingMode mode) const;
+        bool isNumeric(const std::string &string) const;
+        bool isAlphanumeric(const std::string &string) const;
+        
+        const std::string_view alphanumericChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
 };
