fefecde32d7e60c6baab0c3a37ce5583988875a9
Lucas Pujol <lucas.pujol@epitech.eu>
2026-02-07 22:12:20 +0100

    refactor(tests): replace getVersion calls with version method for consistency

--- tests/test_encoding_mode.cpp
+++ tests/test_encoding_mode.cpp
@@ -3,15 +3,15 @@
 
 Test(EncodingMode, test_encoding_mode_numeric_value) {
     QRCode qr("123456789");
-    cr_assert_eq(qr.identifyEncodingMode(), EncodingMode::Numeric);
+    cr_assert_eq(qr.mode(), EncodingMode::Numeric);
 }
 
 Test(EncodingMode, test_encoding_mode_alphanumeric_value) {
     QRCode qr("HELLO-WORLD");
-    cr_assert_eq(qr.identifyEncodingMode(), EncodingMode::Alphanumeric);
+    cr_assert_eq(qr.mode(), EncodingMode::Alphanumeric);
 }
 
 Test(EncodingMode, test_encoding_mode_binary_value) {
     QRCode qr("Hello, World!");
-    cr_assert_eq(qr.identifyEncodingMode(), EncodingMode::Binary);
+    cr_assert_eq(qr.mode(), EncodingMode::Binary);
 }
--- tests/test_version_selection.cpp
+++ tests/test_version_selection.cpp
@@ -3,33 +3,33 @@
 
 Test(VersionSelection, test_version_selection_numeric) {
     QRCode qr("123456789", CorrectionLevel::M);
-    cr_assert_eq(qr.getVersion(), 1);
+    cr_assert_eq(qr.version(), 1);
 }
 
 Test(VersionSelection, test_version_selection_alphanumeric) {
     QRCode qr("HELLO-WORLD", CorrectionLevel::M);
-    cr_assert_eq(qr.getVersion(), 1);
+    cr_assert_eq(qr.version(), 1);
 }
 
 Test(VersionSelection, test_version_selection_binary) {
     QRCode qr("Hello, World!", CorrectionLevel::M);
-    cr_assert_eq(qr.getVersion(), 1);
+    cr_assert_eq(qr.version(), 1);
 }
 
 Test(VersionSelection, test_version_selection_exceed_capacity) {
     std::string longString(5000, 'A');
     QRCode qr(longString, CorrectionLevel::M);
-    cr_assert_eq(qr.getVersion(), -1);
+    cr_assert_eq(qr.version(), -1);
 }
 
 Test(VersionSelection, test_version_selection_version_6_alphanumeric_q) {
     std::string longString(108, 'A');
     QRCode qr(longString, CorrectionLevel::Q);
-    cr_assert_eq(qr.getVersion(), 6);
+    cr_assert_eq(qr.version(), 6);
 }
 
 Test(VersionSelection, test_version_selection_maximum_version_numeric_L) {
     std::string longString(7089, '1');
     QRCode qr(longString, CorrectionLevel::L);
-    cr_assert_eq(qr.getVersion(), 40);
+    cr_assert_eq(qr.version(), 40);
 }
