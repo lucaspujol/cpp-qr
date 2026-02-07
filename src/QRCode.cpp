105f3f273635027f4cd9e13f39e21bb1713deb9d
Lucas Pujol <lucas.pujol@epitech.eu>
2026-02-07 21:36:02 +0100

    feat(QRCode): added getVersion method using the qrcode table file

--- src/QRCode.cpp
+++ src/QRCode.cpp
@@ -1,17 +1,23 @@
 #include "QRCode.hpp"
 #include <iostream>
 #include <algorithm>
+#include "QRCodeTables.hpp"
 
-QRCode::QRCode(const std::string &src) {
-    _src = src;
+QRCode::QRCode(const std::string &src, CorrectionLevel correctionLevel)
+: _src(src), _correctionLevel(correctionLevel) {
+    _mode = identifyEncodingMode();
+    _version = getVersion();
 }
 
 QRCode::~QRCode() {}
 
 void QRCode::generate() {
     std::cout << "generating qrcode for: " << _src << std::endl;
-    _mode = identifyEncodingMode();
     std::cout << "encoding mode: " << EncodingModeToString(_mode) << std::endl;
+    if (_version == -1)
+        std::cerr << "Error: Input data exceeds maximum capacity for any QR code version." << std::endl;
+    else
+        std::cout << "selected version: " << _version << std::endl;
 }
 
 void QRCode::save(const std::string &filepath) {
@@ -22,6 +28,19 @@ EncodingMode QRCode::identifyEncodingMode() const {
     if (isNumeric(_src))        return EncodingMode::Numeric;
     if (isAlphanumeric(_src))   return EncodingMode::Alphanumeric;
     return EncodingMode::Binary;
+}
+
+int QRCode::getVersion() const {
+    int charCount = _src.size();
+    for (int version = 0; version < 40; version++) {
+        int modeIndex = static_cast<int>(_mode);
+        int ecIndex = static_cast<int>(_correctionLevel);
+
+        if (charCount <= CHAR_CAPACITIES[version][ecIndex][modeIndex]) {
+            return version + 1;
+        }
+    }
+    return -1; // Not enough capacity for the given input
 }
 
 bool QRCode::isAlphanumeric(const std::string &string) const {
--- src/QRCode.hpp
+++ src/QRCode.hpp
@@ -8,31 +8,44 @@ enum class EncodingMode {
     Binary,
 };
 
+/**
+ * Each correction level corresponds to a percentage of codewords
+ * that can be restored:
+ * - L: 7% 
+ * - M: 15% 
+ * - Q: 25% 
+ * - H: 30% 
+ * 
+ * Be aware higher levels of error correction require more bytes,
+ * so the higher the error correction level, the larger the QR code
+ * will have to be.
+ */
 enum class CorrectionLevel {
-    L = 7,
-    M = 15,
-    Q = 25,
-    H = 30,
+    L, M, Q, H,
 };
 
 class QRCode {
     public:
-        QRCode(const std::string &src);
+        QRCode(const std::string &src, CorrectionLevel correctionLevel = CorrectionLevel::M);
         ~QRCode();
 
         void generate();
         void save(const std::string &filepath);
+
         EncodingMode identifyEncodingMode() const;
+        int getVersion() const;
+
+        std::string getData() const { return _src; }
 
     private:
         std::string _src;
-        EncodingMode _mode;
         CorrectionLevel _correctionLevel;
-        int _version;
-
+        
+        EncodingMode _mode;
         std::string EncodingModeToString(EncodingMode mode) const;
         bool isNumeric(const std::string &string) const;
         bool isAlphanumeric(const std::string &string) const;
-        
         const std::string_view alphanumericChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
+        
+        int _version;
 };
